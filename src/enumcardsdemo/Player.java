2cd5736e220a5027054b2aa26c6a8ea4c0cb13f8
Joey <camph@sheridancollege.ca>
2019-03-04 17:46:03 -0500

    Added abstract player class

---
+++ src/enumcardsdemo/Player.java
@@ -0,0 +1,13 @@
+
+package enumcardsdemo;
+
+/**
+ *
+ * @author Joey
+ */
+abstract class Player {
+    private int players;
+    
+    // this is considering that the game is played between 2players, Human and AI?
+
+}
